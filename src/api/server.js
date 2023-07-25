0350463d562ffd66e62118a3d640fcf2d3a371d7
prabal <prabalrai17@gmail.com>
2023-07-25 11:25:07 +0545

    first push on aws amplify through git repo

--- src/api/server.js
+++ src/api/server.js
@@ -5,7 +5,8 @@ import axios from "axios";
 //   : "http://localhost:8080/api/v1";
 
 // export const BASE_URL = "https://backendv1.sojonews.com/api/v1";
-export const BASE_URL = "http://localhost:9000/api/v1";
+// export const BASE_URL = "http://localhost:9000/api/v1";
+export const BASE_URL = "https://backendv1.sojonews.com/api/v1";
 export default axios.create({
   baseURL: BASE_URL,
 });
